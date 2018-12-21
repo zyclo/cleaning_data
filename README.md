b26ead37978f2f1576bb712c4be820181c5b38cd
zyclo <yashevrywhr@gmail.com>
2018-12-22 04:51:42 +0530

    Tidy Data

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # Getting-and-Cleaning-Data-Week-4-Assignment
-This project includes four files: README.md, codebook.md, run_analysis.R, tidy_data.txt
 
-Followings are done:
+This repo was created to finish the assignment for week 4 of Getting and Cleaning Data Coursera course.
+* First, download and unzip the data file into your R working directory.
+* Second, download the R source code into your R working directory.
+* Finally, execute R source code to generate tidy data file.
 
-1. Download the dataset to my working directory and unzip.
-2. Load the dataset: X_train.txt, X_test.txt, Y_train.txt, Y_test.txt, subject_train.txt, subject_test.txt, features.txt, activity_labels.txt. 
-3. Merges the X_train and X_test.txt.
-4. Merges the Y_train and Y_test.txt.
-5. Merges the subject_train and subject_test.
-6. Extracts only the measurements on the mean and standard deviation for each measurement.
-7. Uses descriptive activity names to name the activities in the data set.
-8. Appropriately labels the data set with descriptive variable names.
-9. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+### Data description
+The variables in the data X are sensor signals measured with waist-mounted smartphone from 30 subjects. The variable in the data Y indicates activity type the subjects performed during recording.
+
+### Code explaination
+The code combined training dataset and test dataset,  and extracted partial variables to create another dataset with the averages of each variable for each activity.
+
+### New dataset
+The new generated dataset contained variables calculated based on the mean and standard deviation. Each row of the dataset is an average of each activity type for all subjects.
+
+### The code was written based on the instruction of this assignment
+Read training and test dataset into R environment.
+Read variable names into R envrionment.
+Read subject index into R environment.
+
+1. Merges the training and the test sets to create one data set.
+Use command rbind to combine training and test set
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+Use grep command to get column indexes for variable name contains "mean()" or "std()"
+3. Uses descriptive activity names to name the activities in the data set
+Convert activity labels to characters and add a new column as factor
+4. Appropriately labels the data set with descriptive variable names.
+Give the selected descriptive names to variable columns
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+Use pipeline command to create a new tidy dataset with command group_by and summarize_each in dplyr package
--- run_analysis.R
+++ run_analysis.R
@@ -1,38 +1,41 @@
 library(dplyr)
 
-# read data
+# read train data
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+# read test data
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-features <- read.table("./UCI HAR Dataset/features.txt")
+Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+# read data description
+variable_names <- read.table("./UCI HAR Dataset/features.txt")
+
+# read activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# Merges the training and the test sets to create one data set.
-X <- rbind(X_train, X_test)
-Y <- rbind(Y_train, Y_test)
-sub_all <- rbind(sub_train, sub_test)
+# 1. Merges the training and the test sets to create one data set.
+X_total <- rbind(X_train, X_test)
+Y_total <- rbind(Y_train, Y_test)
+Sub_total <- rbind(Sub_train, Sub_test)
 
-# Extracts only the measurements on the mean and standard deviation for each measurement
-features_selected <- features[grep("mean|std",features[,2]),]
-X <- X[,features_selected[,1]]
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_total <- X_total[,selected_var[,1]]
 
-# Uses descriptive activity names to name the activities in the data set
-colnames(Y) <- "label"
-Y$activity <- factor(Y$label, labels = as.character(activity_labels[,2]))
-activity <- Y$activity
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
+activitylabel <- Y_total[,-1]
 
-# Appropriately labels the data set with descriptive variable names
-colnames(X) <- features[features_selected[,1],2]
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- variable_names[selected_var[,1],2]
 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-colnames(sub_all) <- "subject"
-#subject=sub_all$subject
-combine <- cbind(X, activity, sub_all)
-temp <- group_by(combine,activity, subject)
-final <- summarize_all(temp,funs(mean))
-
-# write final tidy data
-write.table(final, file = "./tidy_data.txt", row.names = FALSE, col.names = TRUE)
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average
+# of each variable for each activity and each subject.
+colnames(Sub_total) <- "subject"
+total <- cbind(X_total, activitylabel, Sub_total)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
